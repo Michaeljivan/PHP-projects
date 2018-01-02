62638b9608ef1e481558dab0484c9d54f27c424f
Michael Jivan <32310825+Michaeljivan@users.noreply.github.com>
2018-01-02 13:12:49 -0800

    Update HelloWorld.php

--- HelloWorld.php
+++ HelloWorld.php
@@ -1,3 +1,14 @@
 <?php
   echo "Hello World!";
 ?>
+<html>
+  <head>
+    <title>phpDemo</title>    
+  </head>
+  <body>
+     ...
+  </body>
+  <footer>
+    Michael Jivan @ 2018
+  </footer>
+</html>
